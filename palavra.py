0ff75e4d04ea5cdba62f16bcca5097667539fe76
Victor Matheus de Castro Geraldo <victor@simbioseventures.com>
2018-02-16 13:42:18 -0300

    Add the 'conjugacao' method

--- palavra.py
+++ palavra.py
@@ -37,3 +37,23 @@ class Dicionario:
             significados = [fix_text(significado.strip()) for significado in significados[0: -2]]
             return significados
         return {'status': 'Não encontrado', 'url': url}
+
+    @staticmethod
+    def conjugacao(verbo):
+        url = 'https://www.conjugacao.com.br/verbo-' + unidecode(verbo) + '/'
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
+        verbo = tree.xpath('/html/body/div/div[2]/div/h1//text()')
+        if 'Página Não Encontrada' not in verbo:
+            conjugar = tree.xpath('/html/body/div/div[2]/div/div[2]/div[1]/div/div//text()')
+            tempos_verbais = ['Presente', 'Pretérito Imperfeito', 'Pretérito Perfeito', 'Pretérito Mais-que-perfeito', 'Futuro do Presente', 'Futuro do Pretérito']
+            pessoas = ['eu', 'tu', 'ele', 'nós', 'vós', 'eles']
+            conjugar = [c for c in conjugar if c not in pessoas]
+            lista_indices = [conjugar.index(tempo) for tempo in tempos_verbais]
+            lista_indices.append(len(conjugar))
+            conjugacao = {}
+            for item in range(len(lista_indices)-1):
+                lista = conjugar[lista_indices[item]+1:lista_indices[item+1]]
+                conjugacao[conjugar[lista_indices[item]]] = lista
+            return conjugacao
+        return {'status': 'Não encontrado', 'url': url}
